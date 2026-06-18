2098ca22bf589a02dbdb226cabcaf1885482e7ca
Umesh8907 <umesh.tandon.51@gmail.com>
2026-06-19 01:44:54 +0530

    feat: migrate quest system to templates and add comprehensive user engagement and expert consultation schemas

--- prisma/migrations/20260618194211_remove_school_models/migration.sql
+++ prisma/migrations/20260618194211_remove_school_models/migration.sql
@@ -10,19 +10,6 @@
   - A unique constraint covering the columns `[userId,badgeId]` on the table `UserBadge` will be added. If there are existing duplicate values, this will fail.
 
 */
--- AlterEnum
--- This migration adds more than one value to an enum.
--- With PostgreSQL versions 11 and earlier, this is not possible
--- in a single migration. This can be worked around by creating
--- multiple migrations, each migration adding only one value to
--- the enum.
-
-
-ALTER TYPE "UserRole" ADD VALUE 'EXPERT';
-ALTER TYPE "UserRole" ADD VALUE 'ADMIN';
-ALTER TYPE "UserRole" ADD VALUE 'PEER';
-ALTER TYPE "UserRole" ADD VALUE 'OPS_MANAGER';
-ALTER TYPE "UserRole" ADD VALUE 'SCHOOL_COORDINATOR';
 
 -- DropForeignKey
 ALTER TABLE "UserQuest" DROP CONSTRAINT "UserQuest_questId_fkey";
